de33eb699306903176ddbb76827129bf899c90d0
Kira <kira68784@gmail.com>
2023-12-21 13:21:07 +0800

    learn using pydantic model

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
-@app.get("/")
-async def index():
-    return {"data": {"name": "budi"}}
+@app.get("/blog")
+async def index(limit=10, published: bool = True, sort: str | None = None):
+    if published:
+        return {"data": f"{limit} published blogs from the db"}
+    else:
+        return {"data": f"{limit} blogs from the db"}
 
-@app.get("/about")
-async def about():
-    return {"data": "About page"}
+
+@app.get("/blog/unpublished")
+async def unpublished():
+    return {"data": "all unpublished blog"}
+
+
+@app.get("/blog/{id}")
+async def show(id: int):
+    return {"data": id}
+
+
+@app.get("/blog/{id}/comments")
+async def comments(id: int, limit=10):
+    return {"data": ["1", "2"]}
+
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: bool | None
+
+
+@app.post("/blog")
+async def create_blog(blog: Blog):
+    return {"data": f"Blog is created with title as {blog.title}"}
