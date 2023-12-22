d5c210dc614eacba6d2d311edb0b0232d91b7ea1
Kira <kira68784@gmail.com>
2023-12-22 09:46:40 +0800

    learn add blog to db

--- blog/main.py
+++ blog/main.py
@@ -1,7 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
 
 
 app = FastAPI()
@@ -10,6 +11,19 @@ app = FastAPI()
 models.Base.metadata.create_all(engine)
 
 
+def get_db():
+    db = SessionLocal()
+
+    try:
+        yield db
+    finally:
+        db.close()
+
+
 @app.post("/blog")
-async def create(request: schemas.Blog):
-    return request
+async def create(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
