482200213bc80b94f0886f62fa1bfb0c7caa8490
Kira <kira68784@gmail.com>
2023-12-23 11:03:06 +0800

    add update and delete

--- blog/main.py
+++ blog/main.py
@@ -30,14 +30,31 @@ async def create(request: schemas.Blog, db: Session = Depends(get_db)):
 
 
 @app.delete("/blog/{id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete(id, db: Session = Depends(get_db)):
-    blog = (
-        db.query(models.Blog)
-        .filter(models.Blog.id == id)
-        .delete(synchronize_session=False)
-    )
+async def destroy(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found"
+        )
+
+    blog.delete(synchronize_session=False)
     db.commit()
     return "done"
+
+
+@app.put("/blog/{id}", status_code=status.HTTP_202_ACCEPTED)
+async def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id)
+
+    if not blog.first():
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found"
+        )
+
+    blog.update(request.model_dump())
+    db.commit()
+    return "updated"
 
 
 @app.get("/blog")
