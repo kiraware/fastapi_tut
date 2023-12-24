611b79e2652c3453e5a36a34c08138e6f2f2430e
Kira <kira68784@gmail.com>
2023-12-24 10:33:01 +0800

    create User model and create_user api

--- blog/main.py
+++ blog/main.py
@@ -35,7 +35,8 @@ async def destroy(id, db: Session = Depends(get_db)):
 
     if not blog.first():
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with id {id} not found",
         )
 
     blog.delete(synchronize_session=False)
@@ -49,7 +50,8 @@ async def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
 
     if not blog.first():
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id {id} not found"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Blog with id {id} not found",
         )
 
     blog.update(request.model_dump())
@@ -74,3 +76,16 @@ async def show(id, response: Response, db: Session = Depends(get_db)):
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {"detail": f"Blog with the id {id} is not available"}
     return blog
+
+
+@app.post("/user", status_code=status.HTTP_201_CREATED)
+async def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user = models.User(
+        name=request.name,
+        email=request.email,
+        password=request.password,
+    )
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
