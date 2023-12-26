ef74b845f4374f31d1407ac0a6e01dca998eef7b
Kira <kira68784@gmail.com>
2023-12-26 10:39:25 +0800

    use JWT for authentication

--- blog/routers/authentication.py
+++ blog/routers/authentication.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+from ..token import ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
@@ -14,16 +16,21 @@ def login(request: schemas.Login, db: Session = Depends(get_db)):
 
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Invalid Credentials",
+            headers={"WWW-Authenticate": "Bearer"},
         )
 
     if not Hash.verify(request.password, user.password):
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
+            status_code=status.HTTP_401_UNAUTHORIZED,
             detail=f"Incorrect password",
+            headers={"WWW-Authenticate": "Bearer"},
         )
 
-    # generate a JWT token and return
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
 
-    return user
+    return {"access_token": access_token, "token_type": "bearer"}
